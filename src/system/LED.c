8ce5f98d74cffe68c6d525e01bbe3fe14b3b800e
Harry Parkes <harrydparkes@protonmail.com>
2022-12-08 09:07:27 +1300

    new LED and button modules

--- src/system/LED.c
+++ src/system/LED.c
@@ -1,1 +1,41 @@
 #include "system/LED.h"
+
+struct LED_t {
+    pin_t pin;
+    state_t state;
+};
+
+/**
+ * @brief Constructs a new `LED_t` object
+ * @param port a `port_t` enum value describing the port in which the LED is connected to
+ * @param port_bit the number of the bit within the port in which the LED is connected to
+ * @param mode either `OUTPUT` or `OUTPUT_ACTIVE_LOW` depending on the type of button
+ * @return `LED_t` object to control LED with
+ */
+LED_t LED_construct(port_t port, uint8_t port_bit, pin_mode_t mode)
+{
+    return (LED_t) {
+        .pin = define_pin(port, port_bit, mode)
+    };
+}
+
+/**
+ * @brief Sets the state of the LED to a specified state
+ * @param LED Pointer to the `LED_t` to set the state of
+ * @param state The state to set the LED to
+ */
+void LED_set_state(LED_t* LED, state_t state)
+{
+    digital_write(&LED->pin, state);
+    LED->state = state;
+}
+
+/**
+ * @brief Toggle the state of an LED
+ * @param LED Pointer to the `LED_t` to toggle
+ */
+void LED_toggle_state(LED_t* LED)
+{
+    digital_write(&LED->pin, !(LED->state));
+    LED->state = !(LED->state);
+}
--- src/system/button.c
+++ src/system/button.c
@@ -1,1 +1,62 @@
 #include "system/button.h"
+
+struct button_t {
+    pin_t pin;
+    state_t state;
+    state_t prev_state;
+};
+
+/**
+ * @brief Constructs a new `button_t` object
+ * @param port a `port_t` enum value describing the port in which the button is connected to
+ * @param port_bit the number of the bit within the port in which the button is connected to
+ * @param mode either `INPUT` or `INPUT_PULLUP` depending on the type of button
+ * @return `button_t` object to control the button with
+ */
+button_t button_construct(port_t port, uint8_t port_bit, pin_mode_t mode)
+{
+    return (button_t) {
+        .pin = define_pin(port, port_bit, mode)
+    };
+}
+
+/**
+ * @brief Updates the button's state
+ * @param button Pointer to the `button_t` to update
+ * @return the current state of the button
+ */
+state_t button_update_state(button_t* button)
+{
+    button->prev_state = button->state;
+    button->state = digital_read(&button->pin);
+}
+
+/**
+ * @brief Returns whether or not `button` is pressed down
+ * @param button Pointer to the `button_t` to read from
+ * @return `true` if button is pressed down, `false` otherwise
+ */
+bool button_being_pressed(button_t* button)
+{
+    return button->state == HIGH;
+}
+
+/**
+ * @brief Returns whether or not `button` was just released
+ * @param button Pointer to the `button_t` to read from
+ * @return `true` if button has just been released, `false` otherwise
+ */
+bool button_released_event(button_t* button)
+{
+    return (button->state != button->prev_state) && button->state == LOW;
+}
+
+/**
+ * @brief Returns whether or not `button` was just pressed
+ * @param button Pointer to the `button_t` to read from
+ * @return `true` if button has just been pressed, `false` otherwise
+ */
+bool button_pushed_event(button_t* button)
+{
+    return (button->state != button->prev_state) && button->state == HIGH;
+}
