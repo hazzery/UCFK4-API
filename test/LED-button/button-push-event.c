7a8504a0d52dcd980cece1ff2cb86f1679ca6f67
Harry Parkes <harrydparkes@protonmail.com>
2022-12-12 08:22:48 +1300

    fixed logic in button-push-event

--- test/LED-button/button-push-event.c
+++ test/LED-button/button-push-event.c
@@ -10,8 +10,12 @@ int main(void)
     button_t pushy = button_construct(PORT_D, 7, INPUT);
     LED_t bluey = LED_construct(PORT_C, 2, OUTPUT);
 
-    if (button_pushed_event(&pushy)) {
-        LED_set_state(&bluey, HIGH);
+    while (true) {
+        button_update_state(&pushy);
+
+        if (button_pushed_event(&pushy)) {
+            LED_set_state(&bluey, HIGH);
+        }
     }
     return 0;
 }
