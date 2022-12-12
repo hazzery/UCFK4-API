258b43ed1963006be9d0eafd5e12f22638389f2a
Harry Parkes <harrydparkes@protonmail.com>
2022-12-12 16:16:47 +1300

    Presetup button1 driver module

---
+++ include/drivers/button1.h
@@ -0,0 +1,12 @@
+//
+// Created by Harry on 12/12/22.
+//
+
+#include "system/button.h"
+
+#ifndef BUTTON1_H
+#define BUTTON1_H
+
+extern button_t button1;
+
+#endif //BUTTON1_H
---
+++ src/drivers/button1.c
@@ -0,0 +1,8 @@
+//
+// Created by Harry on 12/12/22.
+//
+
+#include "drivers/button1.h"
+
+button_t button1 = {PORT_D, 7, INPUT};
+
