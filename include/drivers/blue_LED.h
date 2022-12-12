9a03dd303e0ee284a293f273fcbf9d4020cec41b
Harry Parkes <harrydparkes@protonmail.com>
2022-12-12 17:56:52 +1300

    Trying to make blue LED driver

---
+++ include/drivers/blue_LED.h
@@ -0,0 +1,8 @@
+#ifndef BLUE_LED_DRIVER_H
+#define BLUE_LED_DRIVER_H
+
+#include "system/LED.h"
+
+extern LED_t blue_LED;
+
+#endif //BLUE_LED_DRIVER_H
---
+++ src/drivers/blue_LED.c
@@ -0,0 +1,3 @@
+#include "drivers/blue_LED.h"
+
+LED_t blue_LED = LED_construct(PORT_C, 2, OUTPUT);
